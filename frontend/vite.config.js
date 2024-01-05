efb5df04780a58d63554e54f58d17fda7567a858
Jurand Plaskota <jurand.plaskota@gmail.com>
2024-01-05 19:51:19 +0100

    add test project, docker  ✔

---
+++ frontend/vite.config.js
@@ -0,0 +1,14 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
