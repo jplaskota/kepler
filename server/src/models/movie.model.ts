54fcbc121f0e7a86a78f043e380d9f27145838ff
Jurand Plaskota <jurand.plaskota@gmail.com>
2024-08-05 03:27:47 +0200

    add search, raw movie model

--- server/src/models/movie.model.ts
+++ server/src/models/movie.model.ts
@@ -1,5 +1,32 @@
 import { z } from "zod";
 
+const genreSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+});
+
+export const movieSearchSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  release_date: z.string(),
+  popularity: z.number(),
+  poster_path: z.string(),
+  vote_average: z.number(),
+});
+
+export const movieRawSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  runtime: z.number(),
+  release_date: z.string(),
+  genres: z.array(genreSchema),
+  overview: z.string(),
+  homepage: z.string(),
+  poster_path: z.string(),
+  popularity: z.number(),
+  vote_average: z.number(),
+});
+
 export const movieSchema = z.object({
   id: z.string().nanoid({ message: "Invalid id" }),
   tmdb_id: z.string(),
@@ -11,10 +38,11 @@ export const movieSchema = z.object({
   homepage: z.string(),
   poster_path: z.string(),
   popularity: z.number(),
-  status: z.string(),
   vote_average: z.number(),
   media_type: z.enum(["movie", "tv"], { message: "Invalid media type" }),
   added_date: z.number().positive({ message: "Invalid date" }),
 });
 
 export type Movie = z.infer<typeof movieSchema>;
+export type MovieSearch = z.infer<typeof movieSearchSchema>;
+export type MovieRaw = z.infer<typeof movieRawSchema>;
