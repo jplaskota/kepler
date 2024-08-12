2eb05dca408297174fa2cfd0a857b0ad1371ad3c
Jurand Plaskota <jurand.plaskota@gmail.com>
2024-08-12 05:14:15 +0200

    one card for search item

--- client/src/components/Search/SearchItemCard.tsx
+++ client/src/components/Search/SearchItemCard.tsx
@@ -5,18 +5,31 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { cn } from "@/utils/utils";
+import type { MovieSearch } from "@server-models/movie.model";
 import type { SeriesSearch } from "@server-models/series.model";
 import { useState } from "react";
 import { Skeleton } from "../ui/skeleton";
 
-interface SeriesCardProps {
-  item: SeriesSearch;
-  search?: boolean;
+interface MovieCardProps {
+  item: MovieSearch | SeriesSearch;
 }
 
-export default function SearchSeriesCard({ item }: SeriesCardProps) {
+export default function SearchItemCard({ item }: MovieCardProps) {
   const [loading, setLoading] = useState<boolean>(true);
+
+  // Check if the item is a Series or a Movie
+  const isSeries = item.media_type === "tv";
+
+  // Construct poster URL
   const posterUrl = import.meta.env.VITE_IMAGE_BASE_URL + item.poster_path;
+
+  // Determine the title and other properties based on the type
+  const title = isSeries
+      ? (item as SeriesSearch).name
+      : (item as MovieSearch).title,
+    releaseDate = isSeries
+      ? (item as SeriesSearch).first_air_date
+      : (item as MovieSearch).release_date;
 
   return (
     <Card className="select-none">
@@ -25,15 +38,15 @@ export default function SearchSeriesCard({ item }: SeriesCardProps) {
         <img
           src={posterUrl}
           alt="poster"
-          loading="lazy"
+          loading="eager"
           className={cn(loading && "h-0 w-0")}
           onLoad={() => setLoading(false)}
         />
         <CardTitle className="font-Anton text-sm sm:text-2xl sm:pt-1">
-          {item.name.toUpperCase()}
+          {title.toUpperCase()}
         </CardTitle>
         <CardDescription className="max-sm:text-xs">
-          [ {item.first_air_date.split("-")[0]} ]
+          [ {releaseDate.split("-")[0]} ]
         </CardDescription>
       </CardHeader>
     </Card>
--- client/src/components/Search/SearchResult.tsx
+++ client/src/components/Search/SearchResult.tsx
@@ -2,8 +2,7 @@ import { MovieSearch } from "@server-models/movie.model";
 import { SeriesSearch } from "@server-models/series.model";
 import { useEffect, useRef } from "react";
 import { Separator } from "../ui/separator";
-import SearchMovieCard from "./SearchMovieCard";
-import SearchSeriesCard from "./SearchSeriesCard";
+import SearchItemCard from "./SearchItemCard";
 
 interface SearchResultProps {
   results: {
@@ -29,13 +28,13 @@ export default function SearchResult({ results }: SearchResultProps) {
       <div className="w-fit flex flex-col gap-4" ref={searchContainerRef}>
         <div className="w-fit max-w-[90vw] h-fit grid grid-cols-3 sm:grid-cols-4 gap-2">
           {results.movies.map((movie) => {
-            return <SearchMovieCard key={movie.id} item={movie} />;
+            return <SearchItemCard key={movie.id} item={movie} />;
           })}
         </div>
         <Separator orientation="horizontal" />
         <div className="w-fit max-w-[90vw] h-fit grid grid-cols-3 sm:grid-cols-4 gap-2">
           {results.series.map((series) => {
-            return <SearchSeriesCard key={series.id} item={series} />;
+            return <SearchItemCard key={series.id} item={series} />;
           })}
         </div>
       </div>
