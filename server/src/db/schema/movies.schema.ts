00f6067bb5329c61a1ef07ad912e6e8e49836957
Jurand Plaskota <jurand.plaskota@gmail.com>
2025-01-20 08:02:29 +0100

    refactor: reorganize movie and series schemas for improved structure and consistency

--- server/src/db/schema/movies.schema.ts
+++ server/src/db/schema/movies.schema.ts
@@ -1,6 +1,5 @@
 import {
   index,
-  integer,
   pgEnum,
   pgTable,
   text,
@@ -11,34 +10,33 @@ import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 const mediaEnum = pgEnum("media_type", ["movie", "series"]);
 
-// Create the series table
-export const seriesTable = pgTable(
-  "series",
+// Create the movies table
+export const moviesTable = pgTable(
+  "movies",
   {
     id: uuid("id").primaryKey().defaultRandom(),
-    imdb_id: text("tmdb_id").notNull(),
+    imdb_id: text("imdb_id").notNull(),
     title: text("title").notNull(),
     released: text("released").notNull(),
     genre: text("genre"),
-    creator: text("creator"),
+    director: text("director"),
     actors: text("actors"),
     plot: text("plot"),
     country: text("country"),
     poster: text("poster"),
-    seasons: text("seasons"),
     imdb_rating: text("imdb_rating"),
     metascore: text("metascore"),
     rotten_tomatoes: text("rotten_tomatoes"),
     added_date: timestamp("added_date").notNull().defaultNow(),
-    media_type: mediaEnum("media_type").default("series"),
+    media_type: mediaEnum("media_type").default("movie"),
     user_id: text("user_id").notNull(),
   },
-  (series) => {
+  (movie) => {
     return {
-      userIdIndex: index("name_idx").on(series.user_id),
+      userIdIndex: index("name_idx").on(movie.user_id),
     };
   }
 );
 
-export const insertSeriesSchema = createInsertSchema(seriesTable);
-export const selectSeriesSchema = createSelectSchema(seriesTable);
+export const insertMoviesSchema = createInsertSchema(moviesTable);
+export const selectMoviesSchema = createSelectSchema(moviesTable);
--- server/src/kinde.ts
+++ server/src/kinde.ts
@@ -20,28 +20,30 @@ export const kindeClient = createKindeServerClient(
   }
 );
 
-let store: Record<string, unknown> = {};
-
 export const sessionManager = (c: Context): SessionManager => ({
   async getSessionItem(key: string) {
     const result = getCookie(c, key);
     return result;
   },
+
   async setSessionItem(key: string, value: unknown) {
     const cookieOptions = {
       httpOnly: true,
       secure: true,
       sameSite: "Lax",
     } as const;
+
     if (typeof value === "string") {
       setCookie(c, key, value, cookieOptions);
     } else {
       setCookie(c, key, JSON.stringify(value), cookieOptions);
     }
   },
+
   async removeSessionItem(key: string) {
     deleteCookie(c, key);
   },
+
   async destroySession() {
     ["id_token", "access_token", "user", "refresh_token"].forEach((key) => {
       deleteCookie(c, key);
