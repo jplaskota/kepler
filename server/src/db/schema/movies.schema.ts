f40d2307b6d7534402ffac9e198018f0fe67838f
Jurand Plaskota <jurand.plaskota@gmail.com>
2025-01-21 10:11:46 +0100

    refactor: update movies and series schemas for improved structure and consistency

--- server/src/db/schema/movies.schema.ts
+++ server/src/db/schema/movies.schema.ts
@@ -1,5 +1,6 @@
 import {
   index,
+  integer,
   pgEnum,
   pgTable,
   text,
@@ -8,35 +9,32 @@ import {
 } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
-const mediaEnum = pgEnum("media_type", ["movie", "series"]);
+const mediaEnum = pgEnum("media_type", ["movie", "tv"]);
 
-// Create the movies table
-export const moviesTable = pgTable(
+// Create the movies table ( Home card only )
+export const Movies = pgTable(
   "movies",
   {
-    id: uuid("id").primaryKey().defaultRandom(),
+    _id: uuid("id").primaryKey().defaultRandom(),
+    id: text("tmdb_id").notNull(),
     imdb_id: text("imdb_id").notNull(),
     title: text("title").notNull(),
-    released: text("released").notNull(),
-    genre: text("genre"),
-    director: text("director"),
-    actors: text("actors"),
-    plot: text("plot"),
-    country: text("country"),
-    poster: text("poster"),
-    imdb_rating: text("imdb_rating"),
-    metascore: text("metascore"),
-    rotten_tomatoes: text("rotten_tomatoes"),
+    runtime: integer("runtime").notNull(),
+    release_date: text("release_date").notNull(),
+    genres: text("genres").array().notNull(),
+    poster_path: text("poster_path").notNull(),
+    vote_average: integer("vote_average").notNull(),
+    popularity: integer("popularity").notNull(),
     added_date: timestamp("added_date").notNull().defaultNow(),
-    media_type: mediaEnum("media_type").default("movie"),
+    media_type: mediaEnum("media_type").notNull(),
     user_id: text("user_id").notNull(),
   },
-  (movie) => {
+  (movies) => {
     return {
-      userIdIndex: index("name_idx").on(movie.user_id),
+      userIdIndex: index("name_idx").on(movies.user_id),
     };
   }
 );
 
-export const insertMoviesSchema = createInsertSchema(moviesTable);
-export const selectMoviesSchema = createSelectSchema(moviesTable);
+export const insertMediaSchema = createInsertSchema(Movies);
+export const selectMediaSchema = createSelectSchema(Movies);
--- server/src/db/schema/series.schema.ts
+++ server/src/db/schema/series.schema.ts
@@ -9,28 +9,24 @@ import {
 } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
-const mediaEnum = pgEnum("media_type", ["movie", "series"]);
+const mediaEnum = pgEnum("media_type", ["movie", "tv"]);
 
-// Create the series table
-export const seriesTable = pgTable(
+// Create the series table ( Home card only )
+export const Series = pgTable(
   "series",
   {
-    id: uuid("id").primaryKey().defaultRandom(),
-    imdb_id: text("tmdb_id").notNull(),
-    title: text("title").notNull(),
-    released: text("released").notNull(),
-    genre: text("genre"),
-    creator: text("creator"),
-    actors: text("actors"),
-    plot: text("plot"),
-    country: text("country"),
-    poster: text("poster"),
-    seasons: text("seasons"),
-    imdb_rating: text("imdb_rating"),
-    metascore: text("metascore"),
-    rotten_tomatoes: text("rotten_tomatoes"),
+    _id: uuid("id").primaryKey().defaultRandom(),
+    id: text("tmdb_id").notNull(),
+    imdb_id: text("imdb_id").notNull(),
+    name: text("name").notNull(),
+    first_air_date: text("release_date").notNull(),
+    genres: text("genres").array().notNull(),
+    poster_path: text("poster_path").notNull(),
+    number_of_seasons: integer("seasons").notNull(),
+    vote_average: integer("vote_average").notNull(),
+    popularity: integer("popularity").notNull(),
     added_date: timestamp("added_date").notNull().defaultNow(),
-    media_type: mediaEnum("media_type").default("series"),
+    media_type: mediaEnum("media_type"),
     user_id: text("user_id").notNull(),
   },
   (series) => {
@@ -40,5 +36,5 @@ export const seriesTable = pgTable(
   }
 );
 
-export const insertSeriesSchema = createInsertSchema(seriesTable);
-export const selectSeriesSchema = createSelectSchema(seriesTable);
+export const insertMediaSchema = createInsertSchema(Series);
+export const selectMediaSchema = createSelectSchema(Series);
