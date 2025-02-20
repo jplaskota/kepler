30a71a3fc52f25639b391fd1273d9b3e2ed373a3
Jurand Plaskota <jurand.plaskota@gmail.com>
2025-02-20 03:40:19 +0100

    implement functions to fetch recommendations and similar content

--- client/src/services/additional.services.ts
+++ client/src/services/additional.services.ts
@@ -15,3 +15,35 @@ export const getActors = async (id: string, media: string) => {
     .then((res) => res.json());
   return results;
 };
+
+export const getRecommendations = async (id: string, media: string) => {
+  const results = await additional.recommendations[":id"]
+    .$get({
+      param: { id },
+      query: {
+        type: media,
+      },
+    })
+    .then((res) => {
+      if (!res.ok) throw new Error("Failed to fetch recommendations");
+      return res;
+    })
+    .then((res) => res.json());
+  return results;
+};
+
+export const getSimilar = async (id: string, media: string) => {
+  const results = await additional.similar[":id"]
+    .$get({
+      param: { id },
+      query: {
+        type: media,
+      },
+    })
+    .then((res) => {
+      if (!res.ok) throw new Error("Failed to fetch similar content");
+      return res;
+    })
+    .then((res) => res.json());
+  return results;
+};
